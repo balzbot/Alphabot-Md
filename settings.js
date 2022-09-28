edee69a91fdfcfbdbf0d179318de3a80e346a55a
balzbot <98900576+balzbot@users.noreply.github.com>
2022-09-28 21:32:18 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -26,8 +26,8 @@ global.language = indonesia  //change indonesia to english if you don't understa
 
 // Other
 // Other
-global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.botname = "Balz botz-Mdོ" //namabot kalian
+global.ownername= "ᴹᴿ᭄ Balz Ofcོ ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
 global.myweb2 = "https://linktr.ee/zeeoneofc" //bebas
